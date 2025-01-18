0a0f9479ce8eb442e3438e71cb1a6e50f4ba29b9
foyreet <foyreet@yandex.ru>
2025-01-18 19:29:49 +0300

    Разобрал сортировку слиянием

--- codeforces/vk made/sort.cpp
+++ codeforces/vk made/sort.cpp
@@ -9,7 +9,7 @@ void cin_vector(int &n, vector<int> &a) {
         cin >> a[i];
     }
 }
-// O(n^2)
+// Сортировка вставкой O(n^2)
 void sorting_by_inserts() {
     int n;
     vector<int> a;
@@ -25,8 +25,50 @@ void sorting_by_inserts() {
         cout << a[i] << " ";
     }
 }
+// Сортировка слиянием O(nlogn)
 
+// Функция для слияния двух отсортированных массивов
+vector<int> merge(const vector<int> &a, const vector<int> &b) {
+    int n = a.size();
+    int m = b.size();
+    int i = 0, j = 0;
+    vector<int> c;
 
+    while (i + j < n + m) {
+        if (j == m or (i < n and a[i] < b[j])) {
+            c.push_back(a[i]);
+            i++;
+        } else {
+            c.push_back(b[j]);
+            j++;
+        }
+    }
+    return c;
+}
+// Рекурсивная функция сортировки слиянием
+vector<int> merge_sort(const vector<int> &a) {
+    int n = a.size();
+    if (n == 1) {
+        return a;
+    }
+    int mid = n / 2;
+    vector<int> left(a.begin(), a.begin() + mid);
+    vector<int> right(a.begin() + mid, a.end());
+
+    left = merge_sort(left);
+    right = merge_sort(right);
+
+    return merge(left, right);
+}
+void merge_sorting() {
+    int n;
+    vector<int> a;
+    cin_vector(n, a);
+    vector<int> sorted_a = merge_sort(a);
+    for(int num : sorted_a) {
+        cout << num << " ";
+    }
+}
 int main() {
-    sorting_by_inserts();
+    merge_sorting();
 }
